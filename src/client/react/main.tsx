24497750bb72997950bf6416fed46dcae3fb0594
Dean Zhao <zestxjest@outlook.com>
2016-07-22 09:37:24 +0800

    one main.tsx

--- src/client/react/main.tsx
+++ src/client/react/main.tsx
@@ -1,5 +1,41 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./main.prod');
-} else {
-  module.exports = require('./main.dev');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Store, StoreCreator, createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware    from 'redux-thunk';
+import {rootReducer} from './reducers/Reducers';
+import * as C from './components';
+
+let composeParams: any[] = [];
+if (process.env.NODE_ENV !== 'production') {
+  console.log('dev environment');
+  composeParams.push(C.DevTools.instrument());
 }
+
+const enhancer: any = compose(
+  applyMiddleware(thunkMiddleware),
+  ...composeParams
+);
+
+const initialState = {};
+
+const store = createStore(rootReducer, {}, enhancer);
+
+const devtools = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('enable dev tools')
+    return <C.DevTools/>
+  }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <div>
+        <C.App />
+        {devtools() }
+      </div>
+    </div>
+  </Provider>,
+  document.getElementById('app')
+);
