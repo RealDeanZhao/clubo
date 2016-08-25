20ee64bf9aa412a09b77d0df5f2968204680eb83
Dean Zhao <zestxjest@outlook.com>
2016-08-25 10:59:38 -0700

    use redux devtools chrome extension instead of the vanilla one

--- src/client/react/main.js
+++ src/client/react/main.js
@@ -6,21 +6,11 @@ import {Router, IndexRoute, Route, browserHistory, match} from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 import routes from './routes';
-import reducers from './modules/reducer';
 import * as C from './components';
 import finalCreateStore from './createStore';
 
 const store = finalCreateStore();
-
-// const devtools = () => {
-//   if (process.env.NODE_ENV !== 'production') {
-//     console.log('enable dev tools')
-//     return <C.DevTools/>
-//   }
-// }
-
 const histroy = syncHistoryWithStore(browserHistory, store);
-
 
 ReactDOM.render(
   <Provider store={store}>
